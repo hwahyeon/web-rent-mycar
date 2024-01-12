b7f47a476f4f9db726f30834faa6653c0ea29581
im2187 <im2187@naver.com>
2024-01-12 23:34:13 +0900

    feat: 차종 검색 API 추가

--- backend/src/routes/cars.js
+++ backend/src/routes/cars.js
@@ -2,23 +2,54 @@ const express = require("express");
 const router = express.Router();
 const Car = require("../models/carModel");
 
-// 샘플 데이터 조회
-router.get("/cars", (req, res) => {
-  console.log("GET request received for /cars");
-  const sampleData = {
-    cars: [
-      { model: "Car 1", brand: "Brand 1" },
-      { model: "Car 2", brand: "Brand 2" },
-      { model: "Car 3", brand: "Brand 3" },
-    ],
-  };
-  // JSON 형식으로 데이터 응답
-  res.json(sampleData);
+// 차종 검색
+router.get("/search/:model", async (req, res) => {
+  try {
+    const { model } = req.params;
+    const cars = await Car.find({ model: new RegExp(model, "i") });
+    res.json({ cars });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
 });
 
-router.get("/", (req, res) => {
-  console.log("GET request received for /");
-  res.send("Hello world");
+// 차량 정보 조회 (프론트엔드에서 호출할 때 차량 ID를 이용하여)
+router.get("/cars/:model", async (req, res) => {
+  try {
+    const { model } = req.params;
+    const cars = await Car.find({ model: new RegExp(model, "i") });
+    res.json({ cars });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+//--------------------------------------------------------------우선 위에 2개만 참고!! 아래 소스들은 무시!!
+// 차량 목록 조회
+router.get("/cars", (req, res) => {
+  Car.find({}, (err, cars) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: "Internal server error" });
+    } else {
+      res.json({ cars });
+    }
+  });
+});
+
+// 차량 상세 정보 조회
+router.get("/cars/:carId", (req, res) => {
+  const { carId } = req.params;
+  Car.findById(carId, (err, car) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: "Internal server error" });
+    } else {
+      res.json({ car });
+    }
+  });
 });
 
 // 새로운 차량 추가
@@ -45,18 +76,6 @@ router.post("/cars/register", (req, res) => {
       res.status(500).json({ error: "Failed to add a new car" });
     });
 });
-
-// 차량 목록 조회
-// router.get("/cars", (req, res) => {
-//   Car.find({}, (err, cars) => {
-//     if (err) {
-//       console.error(err);
-//       res.status(500).json({ error: "Internal server error" });
-//     } else {
-//       res.json({ cars });
-//     }
-//   });
-// });
 
 // 차량 상세 정보 조회
 router.get("/cars/:carId", (req, res) => {
